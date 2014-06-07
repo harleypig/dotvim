d61d124819190a85317fc70534db7a7ff09463da
Harley Pig <harleypig@gmail.com>
2014-06-07 04:09:24 +0000

    make syntastic work again

--- .vim/bundle/settings/plugin/settings_syntastic.vim
+++ .vim/bundle/settings/plugin/settings_syntastic.vim
@@ -3,32 +3,26 @@
 "            In .vim/bundle/syntastic
 
 " Global settings
-" Default settings are commented
-let g:syntastic_check_on_open=1
-let g:syntastic_check_on_wq=0
-"let g:syntastic_echo_current_error=1
-"let g:syntastic_enable_signs=1
-let g:syntastic_error_symbol='!!'
-let g:syntastic_style_error_symbol='S!'
-let g:syntastic_warning_symbol='W?'
-let g:syntastic_style_warning_symbol='S?'
-let g:syntastic_enable_balloons=0
-let g:syntastic_enable_highlighting=1
-let g:syntastic_always_populate_loc_list=1
-"let g:syntastic_auto_jump=0
-let g:syntastic_auto_loc_list=1
-"let g:syntastic_loc_list_height=10
-"let g:syntastic_ignore_files
-"let g:syntastic_mode_map
-let g:syntastic_quiet_messages=0
-let g:syntastic_stl_format = '[%E{Error @ %fe (%e)}%B{, }%W{Warning @ %fw (%w)} \lo opens, \lc closes]'
+let g:syntastic_aggregate_errors         = 1
+let g:syntastic_always_populate_loc_list = 1
+let g:syntastic_auto_loc_list            = 1
+let g:syntastic_check_on_open            = 1
+let g:syntastic_check_on_wq              = 0
+let g:syntastic_echo_current_error       = 0
+let g:syntastic_enable_balloons          = 0
+let g:syntastic_error_symbol             = '!!'
+let g:syntastic_stl_format               = '[%E{Err %fe (%e)}%B{, }%W{Warn %fw (%w)} Open \lo Close \lc]'
+let g:syntastic_style_error_symbol       = 'S!'
+let g:syntastic_style_warning_symbol     = 'S?'
+let g:syntastic_warning_symbol           = 'W?'
 
-" Perl settings
-"let g:syntastic_perl_perlcritic_options = '--severity 1'
-"let g:syntastic_perl_perlcritic_thres   = 1
-"let g:syntastic_perl_lib_path = './lib'
-let g:syntastic_perl_checkers=[ 'perl' ]
-let g:syntastic_perl_efm_program='~/.vim/tools/efm_perl.pl'
+" Perl Checker options
+let g:syntastic_enable_perl_checker      = 1
+let g:syntastic_perl_checkers           = [ 'myperl', 'perlcritic' ]
+let g:syntastic_perl_interpreter         = '~/.vim/tools/efm_perl.pl'
+"let g:syntastic_perl_myperl_exec        = '~/.vim/tools/efm_perl.pl'
+let g:syntastic_perl_perlcritic_thres    = 0
+"let g:syntastic_debug                    = 31
 
 noremap <silent> <leader>lc :lclose<CR>
-noremap <silent> <leader>lo :lwindow<CR>
+noremap <silent> <leader>lo :Errors<CR>
--- .vim/tools/efm_perl.pl
+++ .vim/tools/efm_perl.pl
@@ -1,30 +1,16 @@
 #!/usr/bin/perl
 
-# XXX: Add support for podchecker and maybe cover
-
-# This is shamelessly ripped from $VIMRUNTIME/tools/efm_perl.pl (which was last
-# updated in 2001 according to the version history--but I'm willing to accept
-# that someone has probably made changes to it since then.) Check that file for
-# details and historical information.
-
-# Since I can only install this file in systems I'm able to install whatever
-# perl modules I want, I'm going to use some modules that make things a little
-# easier.
-
 # Thanks to:
 #
 # http://blogs.perl.org/users/ovid/2011/01/warningsunused-versus-ppi.html for
 # the 'warnings::unused' trick.
 #
 # https://github.com/Ovid/DB--Color.git for the 'circular::require' trick
-#
-# http://blogs.perl.org/users/ovid/2012/07/integrating-perlcritic-and-vim.html
-# for the idea of separating perlcritic out of the perl -c test.
 
-use 5.008;
+#use 5.008;
 
 use strict;
-use warnings;
+use warnings FATAL => 'all';
 use autodie;
 
 use Cwd;
@@ -32,16 +18,12 @@ use File::Basename;
 use IPC::Run3::Simple;
 
 die "Too many arguments!\n"
-  if @ARGV > 1;
+  if scalar @ARGV > 1;
 
-my $file = shift
+my $file = shift @ARGV
   or die "No filename to check!\n";
 
 my ( $perldoc, $efm, $err, $syserr, @checks );
-
-my $output_format = "%.1s:$file:%d:%s\n";
-
-printwarn( 1, qx/ps -o args= $$/ ) if $DEBUG;
 
 # Grab custom instructions for this file from the file.
 
@@ -51,10 +33,8 @@ printwarn( 1, qx/ps -o args= $$/ ) if $DEBUG;
 #
 # will skip the listed tests. Currently known tests are:
 
-my @tests = qw( circular indirect lint method perlcritic todos uninit unused );
+my @tests = qw( circular indirect lint method uninit unused );
 my %test; @test{ @tests } = ( 1 ) x @tests;
-
-#printwarn( 1, 'testing warnings' );
 
 # Any line that begins with
 #
@@ -66,7 +46,6 @@ my %test; @test{ @tests } = ( 1 ) x @tests;
 ## efm debug
 
 ( $efm, $err, $syserr ) = run3( [ 'grep', '^## efm ', $file ] );
-printerr( 1, $err ) if $err;
 
 my $DEBUG = 0;
 
@@ -88,19 +67,6 @@ if ( ! $syserr ) { # 0 = success because we're dealing with the shell
   }
 }
 
-if ( $DEBUG ) {
-
-  my $tests = join ' ', sort keys %test;
-  printinfo( "using $tests" ) if $DEBUG;
-
-}
-
-# Naive attempt to include working module libraries.
-my $dir = dirname( $file ) . '/lib';
-my $cwd = cwd() . '/lib';
-
-my $error_rx = qr{(.*)\sat\s(.*)\sline\s(\d+)([.]|,?\snear\s["'].*["']?)};
-
 # Error messages to be skipped.
 my @skip = (
 
@@ -113,7 +79,6 @@ my $skip = join q{|}, @skip;
 
 # Grab perldoc location
 ( $perldoc, $err, $syserr ) = run3( [qw( which perldoc )] );
-printerr( 1, $err ) if $err;
 
 if ( ! $syserr ) {
 
@@ -129,7 +94,12 @@ if ( ! $syserr ) {
     push @checks, '-MB::Lint::StrictOO'
       if ! $syserr;
 
-    push @checks, '-MO=Lint,all,oo';
+#    ( undef, undef, $syserr ) = run3( [ @perldoc, 'B::LintSubs'  ] );
+#
+#    push @checks, '-MO=LintSubs'
+#      if ! $syserr;
+
+    push @checks, '-MO=Lint,none,magic-diamond,context,bare-subs,regexp-variables,oo';
 
   }
 
@@ -160,7 +130,6 @@ if ( ! $syserr ) {
   if ( $] < 5.010 && exists $test{ uninit } ) { ## no critic qw( ValuesAndExpressions::ProhibitMagicNumbers )
 
     ( undef, $err, $syserr ) = run3( [ @perldoc, 'uninit' ] );
-    printerr( 1, $err ) if $err;
     push @checks, '-Muninit'
       if ! $syserr;
 
@@ -171,8 +140,10 @@ my $checks = join q{ }, @checks;
 
 my ( $message, $extracted_file, $lineno, $rest );
 
-my $cmd = [ 'perl', '-I', $dir, '-I', $cwd, @checks, '-c', $file ];
-printinfo( "@$cmd" ) if $DEBUG;
+# Naive attempt to include working module libraries.
+my @includes = qw( -I ./lib -I ./lib/auto );
+
+my $cmd = [ 'perl', @includes, @checks, '-c', $file ];
 
 run3( {
 
@@ -183,65 +154,9 @@ run3( {
 
 } );
 
-printerr( 1, "stdout - $err" ) if $err;
-
-my $critique  = exists $test{ perlcritic } || 0;
-my $todos     = exists $test{ todos } || 0;
-my $err_count = 0;
-
 for my $error ( @errors ) {
 
   next if $error =~ /$skip/;
-  $error =~ s/([()])/\\$1/g;
+  print "$error\n";
 
-  if ( ( $message, $extracted_file, $lineno, $rest ) = $error =~ /^$error_rx$/ ) {
-
-    $message .= $rest if ( $rest =~ s/^,?\snear// );
-    printerr( $lineno, $message );
-    $critique = 0;
-    $todos    = 0;
-    $err_count++;
-
-  }
 }
-
-printinfo( "$err_count error(s)" ) if $DEBUG;
-
-# If we have errors, we're not going to worry about perlcritic
-if ( $critique ) {
-
-  my $perlcritic;
-  ( $perlcritic, $err ) = run3( [qw( which perlcritic )] );
-  printerr( 1, $err ) if $err;
-
-  my $format = 'W:%f:%l:%m (near %r) %p\n'; ## no critic qw( ValuesAndExpressions::RequireInterpolationOfMetachars )
-  $cmd = [ $perlcritic, '--quiet', '--verbose', $format, $file ];
-  printinfo( "@$cmd" ) if $DEBUG;
-
-  run3( {
-
-      'cmd'    => $cmd,
-      'stdout' => \my @critiques,
-      'stderr' => \$err,
-
-  } );
-
-  printerr( 1, $err ) if $err;
-  print "$_\n" for @critiques;
-  printinfo( scalar @critiques . 'critiques(s)' ) if $DEBUG;
-  $todos = 0 if @critiques;
-
-} ## end if ( $critique )
-
-# If we have errors or perlcritic has issues, we're not going to worry about todos.
-if ( $todos ) {
-
-## TODO: one
-  # XXX:
-}
-
-sub printline { printf $output_format, @_ }
-
-sub printinfo { printline( 'I', 1, @_ ) }
-sub printwarn { printline( 'W', @_ ) }
-sub printerr  { printline( 'E', @_ ) }
